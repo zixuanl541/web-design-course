1b83d4c391f6a6fdef0a1638410100ccaf69020b
zixuanl541 <zixuanl541@gmail.com>
2026-04-17 03:59:16 +0800

    Delete A1133347_林子瑄_作業3 directory

--- A1133347_林子瑄_作業3/HW3/cookiedelete.php
+++
@@ -1,6 +0,0 @@
-<?php
-setcookie("uName", "", time() - 3600);
-
-echo "<h2>Cookie has been deleted.</h2>";
-echo "<a href='index.php'>Back to Login</a>";
-?>
--- A1133347_林子瑄_作業3/HW3/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();   
-    session_unset(); 
-    session_destroy();  
-    setcookie("uName", "", time() - 3600);
-    header("Location: index.php");
-    exit();
-?>
